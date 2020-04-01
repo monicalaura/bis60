b5f3d5f61c70d5183e818b4059167934dc6ef6fd
monicalaura <monicalauraburns@yahoo.com>
2020-04-01 15:15:18 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+
+ //toggler btn
+  $('.navbar-toggler').click(function () {
+
+    $('.navbar-toggler').toggleClass('change')
+      
+  });
+
+   //sticky navbar
+   $(window).scroll(function () { 
+
+    let position = $(this).scrollTop();
+
+    if(position>= 718){
+       $('.navbar').addClass('nav-anim');
+       $('.navbar').addClass('fixed-top');
+    }
+    else{
+      $('.navbar').removeClass('nav-anim');
+       $('.navbar').removeClass('fixed-top');
+    }
+     
+   });
+
+   //smooth scroll
+   $('.nav-item a, .header-link, .footer-link, .about-btn, .bis-btn').click(function(link){
+    link.preventDefault();
+    let target = $(this).attr('href');
+
+    $('html, body').stop().animate({
+      scrollTop: $(target).offset().top - 25
+    },2000)
+
+   })
+
+   //back to top
+
+   $(window).scroll(function () { 
+
+    let position = $(this).scrollTop();
+
+    if(position>= 718){
+       $('.footer-link').addClass('scrollTop');    
+    }
+    else{
+      $('.footer-link').removeClass('scrollTop');
+    }
+     
+   });
+
+
+    //ripples effect
+    $("#header, .info").ripples({
+        dropRadius: 25,
+        perturbance: 0.7,
+        
+      });
+
+      //magnific popup
+      $('.parent-container').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        // other options
+        gallery: {
+          enabled: true
+        }
+      });
+});
